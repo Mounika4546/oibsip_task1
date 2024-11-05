bd9a5b196e17eeabc22406af9a92155bf90fb33a
Mounika4546 <165478445+Mounika4546@users.noreply.github.com>
2024-11-05 18:09:57 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,3 @@
+document.querySelector('.content-section h2').textContent = "10 Must-Visit Hidden Gems";
+document.querySelector('.content-section p').textContent = "Discover off-the-beaten-path places that offer unforgettable experiences and scenic beauty away from the crowds.";
+
